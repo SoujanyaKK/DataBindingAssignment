d62346690ae632e05cf495a6eea666199f681c37
skulkarni207 <skulkarni207@IN-4H3K9S3>
2024-02-14 13:36:55 +0530

    Add project files.

---
+++ XamarinSampleTwo/XamarinSampleTwo/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinSampleTwo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ XamarinSampleTwo/XamarinSampleTwo/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSampleTwo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1()
+		{
+			InitializeComponent();
+			//Content = new Button
+			//{
+			//	Text = "Start",
+			//	HorizontalOptions = LayoutOptions.Center,
+			//	VerticalOptions = LayoutOptions.Center,
+			//};
+			slider1.Value = 0.5;
+		}
+
+		private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			label1.Text = string.Format("Value is {0:f2}", e.NewValue);
+		}
+
+		private void EntryText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			label2.Text = e.NewTextValue;
+		}
+
+		private void slider4_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			label4.FontSize = slider4.Value;
+		}
+
+	}
+}
